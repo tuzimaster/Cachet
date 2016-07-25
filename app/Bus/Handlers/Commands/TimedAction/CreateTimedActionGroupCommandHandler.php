8ad1f32e4ce748e9ecb6ca5c214109f2d7a3bf84
James Brooks <james@alt-three.com>
2016-07-25 09:12:33 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- app/Bus/Handlers/Commands/TimedAction/CreateTimedActionGroupCommandHandler.php
+++ app/Bus/Handlers/Commands/TimedAction/CreateTimedActionGroupCommandHandler.php
@@ -11,7 +11,6 @@
 
 namespace CachetHQ\Cachet\Bus\Handlers\Commands\TimedAction;
 
-use CachetHQ\Cachet\Bus\Commands\TimedAction\CreateTimedActionGroupCommand;
 use CachetHQ\Cachet\Models\TimedActionGroup;
 
 /**
