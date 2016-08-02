cd6e4a9e79dea7152c3f956493265057e45a7b3e
James Brooks <james@alt-three.com>
2016-08-02 10:50:09 +0100

    Added more timed action command handlers

---
+++ app/Bus/Handlers/Commands/TimedAction/DeleteTimedActionGroupCommandHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Bus\Commands\TimedAction;
+
+use CachetHQ\Cachet\Bus\Commands\TimedAction\DeleteTimedActionGroupCommand;
+
+/**
+ * This is the delete timed action group command handler class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class DeleteTimedActionGroupCommandHandler
+{
+    /**
+     * Handle the delete timed action group command.
+     *
+     * @param \CachetHQ\Cachet\Bus\Commands\TimedAction\DeleteTimedActionGroupCommand $command
+     *
+     * @return void
+     */
+    public function handle(DeleteTimedActionGroupCommand $command)
+    {
+        $command->group->delete();
+    }
+}
