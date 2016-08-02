f7388930256d01f95c0c20da5b99844d833517b9
James Brooks <james@alt-three.com>
2016-08-02 10:49:59 +0100

    Added TimedActionTest class

---
+++ tests/Api/TimedActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Tests\Cachet\Api;
+
+/**
+ * This is the timed action test class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class TimedActionTest extends AbstractApiTestCase
+{
+    //
+}
